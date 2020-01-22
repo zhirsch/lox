71e3932f265e6e0998024d3fabcaecd32ac5f3e8
Zachary Hirsch <zhirsch@umich.edu>
2020-01-21 20:35:06 -0500

    scanToken

--- jlox/src/main/java/com/zacharyhirsch/lox/Scanner.java
+++ jlox/src/main/java/com/zacharyhirsch/lox/Scanner.java
@@ -3,7 +3,17 @@ package com.zacharyhirsch.lox;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.zacharyhirsch.lox.TokenType.COMMA;
+import static com.zacharyhirsch.lox.TokenType.DOT;
 import static com.zacharyhirsch.lox.TokenType.EOF;
+import static com.zacharyhirsch.lox.TokenType.LEFT_BRACE;
+import static com.zacharyhirsch.lox.TokenType.LEFT_PAREN;
+import static com.zacharyhirsch.lox.TokenType.MINUS;
+import static com.zacharyhirsch.lox.TokenType.PLUS;
+import static com.zacharyhirsch.lox.TokenType.RIGHT_BRACE;
+import static com.zacharyhirsch.lox.TokenType.RIGHT_PAREN;
+import static com.zacharyhirsch.lox.TokenType.SEMICOLON;
+import static com.zacharyhirsch.lox.TokenType.STAR;
 
 final class Scanner {
 
@@ -26,6 +36,22 @@ final class Scanner {
 
     tokens.add(new Token(EOF, "", null, line));
     return tokens;
+  }
+
+  private void scanToken() {
+    char c = advance();
+    switch (c) {
+      case '(': addToken(LEFT_PAREN); break;
+      case ')': addToken(RIGHT_PAREN); break;
+      case '{': addToken(LEFT_BRACE); break;
+      case '}': addToken(RIGHT_BRACE); break;
+      case ',': addToken(COMMA); break;
+      case '.': addToken(DOT); break;
+      case '-': addToken(MINUS); break;
+      case '+': addToken(PLUS); break;
+      case ';': addToken(SEMICOLON); break;
+      case '*': addToken(STAR); break;
+    }
   }
 
   private boolean isAtEnd() {
