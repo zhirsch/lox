b05c7b4c0d9516d4f251a63b004fd2cf09ea3129
Zachary Hirsch <zhirsch@umich.edu>
2020-01-21 20:23:10 -0500

    Initial work on lexer

---
+++ jlox/src/main/java/com/zacharyhirsch/lox/Scanner.java
@@ -0,0 +1,12 @@
+package com.zacharyhirsch.lox;
+
+import com.google.common.collect.ImmutableList;
+
+public class Scanner {
+
+  public Scanner(String source) {}
+
+  public ImmutableList<Token> scanTokens() {
+    return null;
+  }
+}
---
+++ jlox/src/main/java/com/zacharyhirsch/lox/Token.java
@@ -0,0 +1,3 @@
+package com.zacharyhirsch.lox;
+
+public class Token {}
