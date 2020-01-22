36a3f5236cc2dd48c03f4a26f5927abfa7f837d7
Zachary Hirsch <zhirsch@umich.edu>
2020-01-21 20:26:15 -0500

    define token class

--- jlox/src/main/java/com/zacharyhirsch/lox/Token.java
+++ jlox/src/main/java/com/zacharyhirsch/lox/Token.java
@@ -1,3 +1,20 @@
 package com.zacharyhirsch.lox;
 
-public class Token {}
+final class Token {
+
+    private final TokenType type;
+    private final String lexeme;
+    private final Object literal;
+    private final int line;
+
+    Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
