2421c6319167bf4772af9fb9ab76950beeb93b04
Zachary Hirsch <zhirsch@umich.edu>
2020-01-22 11:53:49 -0500

    visitor

--- jlox/src/main/java/com/zacharyhirsch/tool/GenerateAst.java
+++ jlox/src/main/java/com/zacharyhirsch/tool/GenerateAst.java
@@ -36,14 +36,30 @@ public class GenerateAst {
       writer.println("@Generated(\"" + GenerateAst.class.getTypeName() + "\")");
       writer.println("abstract class " + baseName + " {");
 
+      defineVisitor(writer, baseName, types);
+
       for (String type : types) {
         String className = type.split(":")[0].trim();
         String fields = type.split(":")[1].trim();
         defineType(writer, baseName, className, fields);
       }
 
+      writer.println();
+      writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
       writer.println("}");
     }
+  }
+
+  private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+    writer.println("  interface Visitor<R> {");
+
+    for (String type : types) {
+      String typeName = type.split(":")[0].trim();
+      writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+    }
+
+    writer.println("  }");
   }
 
   private static void defineType(
@@ -51,12 +67,16 @@ public class GenerateAst {
     writer.println("  static class " + className + " extends " + baseName + " {");
 
     writer.println("    " + className + "(" + fields + ") {");
-
     for (String field : fields.split(", ")) {
       String name = field.split(" ")[1];
       writer.println("      this." + name + " = " + name + ";");
     }
+    writer.println("    }");
 
+    writer.println();
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println("      return visitor.visit" + className + baseName + "(this);");
     writer.println("    }");
 
     writer.println();
