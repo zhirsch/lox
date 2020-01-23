4d4d8aad5869b3a2ced69df0225415a8a01a3a65
Zachary Hirsch <zhirsch@umich.edu>
2020-01-22 19:47:13 -0500

    Lox.error overload for Token

--- jlox/src/main/java/com/zacharyhirsch/lox/Lox.java
+++ jlox/src/main/java/com/zacharyhirsch/lox/Lox.java
@@ -50,4 +50,12 @@ final class Lox {
     System.err.println("[line " + line + "] Error" + where + ": " + message);
     hadError = true;
   }
+
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
+  }
 }
--- jlox/src/main/java/com/zacharyhirsch/lox/Token.java
+++ jlox/src/main/java/com/zacharyhirsch/lox/Token.java
@@ -5,7 +5,7 @@ final class Token {
     final TokenType type;
     final String lexeme;
     final Object literal;
-    private final int line;
+    final int line;
 
     Token(TokenType type, String lexeme, Object literal, int line) {
         this.type = type;
