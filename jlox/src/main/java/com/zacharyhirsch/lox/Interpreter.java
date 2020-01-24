3c96b01e25bd61262bf2ce698f52996955185a86
Zachary Hirsch <zhirsch@umich.edu>
2020-01-24 08:32:57 -0500

    Interpreter.visitLiteralExpr

--- jlox/src/main/java/com/zacharyhirsch/lox/Interpreter.java
+++ jlox/src/main/java/com/zacharyhirsch/lox/Interpreter.java
@@ -1,4 +1,9 @@
 package com.zacharyhirsch.lox;
 
 public class Interpreter implements Expr.Visitor<Object> {
+
+  @Override
+  public Object visitLiteralExpr(Expr.Literal expr) {
+    return expr.value;
+  }
 }
