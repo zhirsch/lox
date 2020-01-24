da28464cda72a6fe0e989f8e15a07747205b7f77
Zachary Hirsch <zhirsch@umich.edu>
2020-01-24 08:33:33 -0500

    Interpreter.visitGroupingExpr

--- jlox/src/main/java/com/zacharyhirsch/lox/Interpreter.java
+++ jlox/src/main/java/com/zacharyhirsch/lox/Interpreter.java
@@ -3,6 +3,11 @@ package com.zacharyhirsch.lox;
 public class Interpreter implements Expr.Visitor<Object> {
 
   @Override
+  public Object visitGroupingExpr(Expr.Grouping expr) {
+    return evaluate(expr.expression);
+  }
+
+  @Override
   public Object visitLiteralExpr(Expr.Literal expr) {
     return expr.value;
   }
