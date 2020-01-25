c37a2ca125083650ea5ed3835c70005809f2f035
Zachary Hirsch <zhirsch@umich.edu>
2020-01-24 23:24:34 -0500

    interpret variables

--- jlox/src/main/java/com/zacharyhirsch/lox/Interpreter.java
+++ jlox/src/main/java/com/zacharyhirsch/lox/Interpreter.java
@@ -1,10 +1,14 @@
 package com.zacharyhirsch.lox;
 
+import com.zacharyhirsch.lox.Expr.Variable;
 import com.zacharyhirsch.lox.Stmt.Expression;
 import com.zacharyhirsch.lox.Stmt.Print;
+import com.zacharyhirsch.lox.Stmt.Var;
 import java.util.List;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+
+  private final Environment environment = new Environment();
 
   void interpret(List<Stmt> statements) {
     try {
@@ -86,6 +90,11 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     return null;
   }
 
+  @Override
+  public Object visitVariableExpr(Variable expr) {
+    return environment.get(expr.name);
+  }
+
   private void checkNumberOperand(Token operator, Object operand) {
     if (operand instanceof Double) {
       return;
@@ -157,6 +166,16 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   public Void visitPrintStmt(Print stmt) {
     Object value = evaluate(stmt.expression);
     System.out.println(stringify(value));
+    return null;
+  }
+
+  @Override
+  public Void visitVarStmt(Var stmt) {
+    Object value = null;
+    if (stmt.initializer != null) {
+      value = evaluate(stmt.initializer);
+    }
+    environment.define(stmt.name.lexeme, value);
     return null;
   }
 }
