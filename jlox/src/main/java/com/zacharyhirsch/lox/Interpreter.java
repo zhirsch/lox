a6f80c932226f466574dd41a4524a1a92c50c247
Zachary Hirsch <zhirsch@umich.edu>
2020-01-24 22:10:33 -0500

    support adding string and non-string

--- jlox/src/main/java/com/zacharyhirsch/lox/Interpreter.java
+++ jlox/src/main/java/com/zacharyhirsch/lox/Interpreter.java
@@ -43,8 +43,8 @@ public class Interpreter implements Expr.Visitor<Object> {
         if (left instanceof Double && right instanceof Double) {
           return (double) left + (double) right;
         }
-        if (left instanceof String && right instanceof String) {
-          return (String) left + (String) right;
+        if (left instanceof String || right instanceof String) {
+          return stringify(left) + stringify(right);
         }
         throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
       case GREATER:
